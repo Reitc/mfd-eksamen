3424d23ef5141b492cb9597d5e1cfee616b229d9
Emilie <mistie@live.dk>
2024-06-18 11:02:35 +0200

    pwa up and working

--- geografiskHave-main/vite.config.js
+++ geografiskHave-main/vite.config.js
@@ -3,11 +3,48 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		VitePWA({ registerType: 'autoUpdate' })
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true,
+			},
+			manifest: {
+				name: 'First PWA App',
+				short_name: 'FirstPWA',
+				description: 'First PWA App',
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+				display: 'standalone',
+				scope: '/',
+				start_url: '/',
+				icons: [
+					{
+						src: '/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: '/maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+					{
+						src: '/apple-touch-icon-180x180.png',
+						sizes: '180x180',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
 	],
 	resolve: {
 		alias: {

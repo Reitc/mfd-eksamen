95330f93b0c243396b37524c2d1de64cf34993b3
Emilie <mistie@live.dk>
2024-06-19 12:30:35 +0200

    Update sw.js

--- geografiskHave-main/dev-dist/sw.js
+++ geografiskHave-main/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0cir2b08tm"
+    "revision": "0.417p6nbpcjo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
